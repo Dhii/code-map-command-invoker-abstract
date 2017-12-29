22375c651c7f3359645f2c3e404c5237b98eac48
Anton Ukhanev <xedin.unknown@gmail.com>
2017-12-29 22:50:40 +0100

    `_mapCallablesToCodes()` now throws if invalid map supplied

--- src/MapCallablesToCodesCapableTrait.php
+++ src/MapCallablesToCodesCapableTrait.php
@@ -2,6 +2,11 @@
 
 namespace Dhii\Invocation;
 
+use InvalidArgumentException;
+use Exception as RootException;
+use Dhii\Util\String\StringableInterface as Stringable;
+use Traversable;
+
 trait MapCallablesToCodesCapableTrait
 {
     /**
@@ -13,6 +18,10 @@ trait MapCallablesToCodesCapableTrait
      */
     protected function _mapCallablesToCodes($map)
     {
+        if (!is_array($map) && !($map instanceof Traversable)) {
+            throw $this->_createInvalidArgumentException($this->__('Not a valid map'));
+        }
+
         foreach ($map as $_code => $_callable) {
             $this->_mapCallableToCode($_code, $_callable);
         }
@@ -29,4 +38,37 @@ trait MapCallablesToCodesCapableTrait
      * @throws InvalidArgumentException If the callable is not valid.
      */
     abstract protected function _mapCallableToCode($code, callable $callable);
+
+    /**
+     * Creates a new invalid argument exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable|null $message  The error message, if any.
+     * @param int|null               $code     The error code, if any.
+     * @param RootException|null     $previous The inner exception for chaining, if any.
+     * @param mixed|null             $argument The invalid argument, if any.
+     *
+     * @return InvalidArgumentException The new exception.
+     */
+    abstract protected function _createInvalidArgumentException(
+        $message = null,
+        $code = null,
+        RootException $previous = null,
+        $argument = null
+    );
+
+    /**
+     * Translates a string, and replaces placeholders.
+     *
+     * @since [*next-version*]
+     * @see   sprintf()
+     *
+     * @param string $string  The format string to translate.
+     * @param array  $args    Placeholder values to replace in the string.
+     * @param mixed  $context The context for translation.
+     *
+     * @return string The translated string.
+     */
+    abstract protected function __($string, $args = [], $context = null);
 }
