98adaf1077901531b410195d476857b6b9242433
Anton Ukhanev <xedin.unknown@gmail.com>
2017-12-30 15:05:52 +0100

    `_invokeByCode()` must now also accept `Traversable` args

--- test/unit/InvokeByCodeCapableTraitTest.php
+++ test/unit/InvokeByCodeCapableTraitTest.php
@@ -2,6 +2,8 @@
 
 namespace Dhii\Invocation\UnitTest;
 
+use ArrayIterator;
+use Traversable;
 use Xpmock\TestCase;
 use Dhii\Invocation\InvokeByCodeCapableTrait as TestSubject;
 
@@ -36,10 +38,27 @@ class InvokeByCodeCapableTraitTest extends TestCase
                 }));
         $mock->method('_invokeCallable')
                 ->will($this->returnCallback(function ($callable, $args) {
+                    $args = $args instanceof Traversable
+                        ? iterator_to_array($args)
+                        : $args;
                     return call_user_func_array($callable, $args);
                 }));
 
         return $mock;
+    }
+
+    /**
+     * Creates a traversable list.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $array The array with elements for the traversable.
+     *
+     * @return Traversable The new Traversable.
+     */
+    public function createTraversable(array $array)
+    {
+        return new ArrayIterator($array);
     }
 
     /**
@@ -75,17 +94,49 @@ class InvokeByCodeCapableTraitTest extends TestCase
         $_subject = $this->reflect($subject);
 
         $subject->expects($this->exactly(1))
-                ->method('_getCallableByCode')
-                ->with($this->equalTo($code));
+            ->method('_getCallableByCode')
+            ->with($this->equalTo($code));
         $subject->expects($this->exactly(1))
-                ->method('_invokeCallable')
-                ->with(
-                    $this->equalTo($callable),
-                    $this->equalTo($args)
-                );
+            ->method('_invokeCallable')
+            ->with(
+                $this->equalTo($callable),
+                $this->equalTo($args)
+            );
 
         $result = $_subject->_invokeByCode($code, $args);
 
-        $this->assertEquals($args, $result, 'Invocation produced a wrong result');
+        $this->assertEquals($args, $result, 'Invocation (array args) produced a wrong result');
+    }
+
+    /**
+     * Tests whether invoking a callable works as expected.
+     *
+     * @since [*next-version*]
+     */
+    public function testInvokeByCodeTraversable()
+    {
+        $code = uniqid('code-');
+        $_args = ['Apple', 'Banana'];
+        $args = $this->createTraversable($_args);
+        $callable = function () {
+            return func_get_args();
+        };
+
+        $subject = $this->createInstance([$code => $callable]);
+        $_subject = $this->reflect($subject);
+
+        $subject->expects($this->exactly(1))
+            ->method('_getCallableByCode')
+            ->with($this->equalTo($code));
+        $subject->expects($this->exactly(1))
+            ->method('_invokeCallable')
+            ->with(
+                $this->equalTo($callable),
+                $this->equalTo($args)
+            );
+
+        $result = $_subject->_invokeByCode($code, $args);
+
+        $this->assertEquals($_args, $result, 'Invocation (traversable args) produced a wrong result');
     }
 }
